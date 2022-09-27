acc0da69bd480ce26d69fb93e8d1cd24c8904d56
Paul Heidekrüger <paul.heidekrueger@in.tum.de>
2022-09-27 17:05:39 +0000

    Build: explicitly set compilers for cmake

--- doitlk-llvm-build.py
+++ doitlk-llvm-build.py
@@ -4,16 +4,15 @@ import sys
 import subprocess
 import os
 
-CMAKE_CONFIG_FLAGS = ["-DLLVM_ENABLE_PROJECTS=\"clang;clang-tools-extra;lldb\"",
-                      "-DLLVM_TARGETS_TO_BUILD=\"AArch64;ARM;X86\"",
-                      "-DLLVM_TARGET_ARCH=\"AArch64\"",
-                      "-DLLVM_DEFAULT_TARGET_TRIPLE=\"aarch64-unknown-linux-gnu\"",
-                      "-DCMAKE_BUILD_TYPE=\"DEBUG\"",
-                      "-DLLVM_ENABLE_ASSERTIONS=\"ON\"",
-                      "-DCMAKE_EXPORT_COMPILE_COMMANDS=\"ON\"",
-                      "-DLLVM_CCACHE_BUILD=\"ON\""
-                      "-DLLVM_ENABLE_LLD=\"ON\""
-                      ]
+CMAKE_CONFIG_FLAGS = [
+    "-DLLVM_ENABLE_PROJECTS=\"clang;clang-tools-extra;lldb\"",
+    "-DLLVM_TARGETS_TO_BUILD=\"AArch64;ARM;X86\"",
+    "-DLLVM_TARGET_ARCH=\"AArch64\"",
+    "-DLLVM_DEFAULT_TARGET_TRIPLE=\"aarch64-unknown-linux-gnu\"",
+    "-DCMAKE_BUILD_TYPE=\"DEBUG\"", "-DLLVM_ENABLE_ASSERTIONS=\"ON\"",
+    "-DCMAKE_EXPORT_COMPILE_COMMANDS=\"ON\"", "-DLLVM_CCACHE_BUILD=\"ON\"",
+    "-DLLVM_ENABLE_LLD=\"ON\"",
+    "-DCMAKE_C_COMPILER=clang", "-DCMAKE_CXX_COMPILER=clang++"]
 
 
 def configure_llvm():
