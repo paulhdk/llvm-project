5cf7041af142e53167e1f5118550784c79c8052c
Paul Heidekrüger <paul.heidekrueger@in.tum.de>
2022-10-09 12:50:13 +0000

    Build: use zlib

--- doitlk-llvm-build.py
+++ doitlk-llvm-build.py
@@ -11,8 +11,8 @@ CMAKE_CONFIG_FLAGS = [
     "-DLLVM_DEFAULT_TARGET_TRIPLE=\"aarch64-unknown-linux-gnu\"",
     "-DCMAKE_BUILD_TYPE=\"DEBUG\"", "-DLLVM_ENABLE_ASSERTIONS=\"ON\"",
     "-DCMAKE_EXPORT_COMPILE_COMMANDS=\"ON\"", "-DLLVM_CCACHE_BUILD=\"ON\"",
-    "-DLLVM_ENABLE_LLD=\"ON\"",
-    "-DCMAKE_C_COMPILER=clang", "-DCMAKE_CXX_COMPILER=clang++"]
+    "-DLLVM_ENABLE_LLD=\"ON\"", "-DCMAKE_C_COMPILER=clang",
+    "-DCMAKE_CXX_COMPILER=clang++", "-DLLVM_ENABLE_ZLIB=\"ON\""]
 
 
 def configure_llvm():
