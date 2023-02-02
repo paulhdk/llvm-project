85d9ca833c21c1c8bb301e901673542698c58d85
Paul Heidekrüger <paul.heidekrueger@in.tum.de>
2023-02-02 14:35:51 +0000

    Build: disable X86 support

--- doitlk-llvm-build.py
+++ doitlk-llvm-build.py
@@ -6,13 +6,13 @@ import os
 
 CMAKE_CONFIG_FLAGS = [
     "-DLLVM_ENABLE_PROJECTS=\"clang;clang-tools-extra;lldb;lld\"",
-    "-DLLVM_TARGETS_TO_BUILD=\"AArch64;ARM;X86\"",
+    "-DLLVM_TARGETS_TO_BUILD=\"AArch64;ARM\"",
     "-DLLVM_TARGET_ARCH=\"AArch64\"",
     "-DLLVM_DEFAULT_TARGET_TRIPLE=\"aarch64-unknown-linux-gnu\"",
     "-DCMAKE_BUILD_TYPE=\"DEBUG\"", "-DLLVM_ENABLE_ASSERTIONS=\"ON\"",
     "-DCMAKE_EXPORT_COMPILE_COMMANDS=\"ON\"", "-DLLVM_CCACHE_BUILD=\"ON\"",
-    "-DLLVM_ENABLE_LLD=\"ON\"", "-DCMAKE_C_COMPILER=clang",
-    "-DCMAKE_CXX_COMPILER=clang++", "-DLLVM_ENABLE_ZLIB=\"ON\""]
+    "-DLLVM_ENABLE_LLD=\"ON\"", "-DCMAKE_C_COMPILER=\"clang\"",
+    "-DCMAKE_CXX_COMPILER=\"clang++\"", "-DLLVM_ENABLE_ZLIB=\"ON\""]
 
 
 def configure_llvm():
