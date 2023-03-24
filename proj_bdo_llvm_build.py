4223d6b7c2feeddc1fd522a6e0d9d5fc5cfde056
Paul Heidekrüger <paul.heidekrueger@in.tum.de>
2023-03-24 13:49:59 +0000

    Build: print executed commands

--- proj_bdo_llvm_build.py
+++ proj_bdo_llvm_build.py
@@ -2,32 +2,33 @@
 
 import sys
 import subprocess
-import os
 
 CMAKE_CONFIG_FLAGS = [
-    "-DLLVM_ENABLE_PROJECTS=\"clang;clang-tools-extra;lldb;lld\"",
-    "-DLLVM_TARGETS_TO_BUILD=\"AArch64;ARM\"",
-    "-DLLVM_TARGET_ARCH=\"AArch64\"",
-    "-DLLVM_DEFAULT_TARGET_TRIPLE=\"aarch64-unknown-linux-gnu\"",
-    "-DCMAKE_BUILD_TYPE=\"DEBUG\"", "-DLLVM_ENABLE_ASSERTIONS=\"ON\"",
-    "-DCMAKE_EXPORT_COMPILE_COMMANDS=\"ON\"", "-DLLVM_CCACHE_BUILD=\"ON\"",
-    "-DLLVM_ENABLE_LLD=\"ON\"", "-DCMAKE_C_COMPILER=\"clang\"",
-    "-DCMAKE_CXX_COMPILER=\"clang++\"", "-DLLVM_ENABLE_ZLIB=\"ON\""]
+    "-DLLVM_ENABLE_PROJECTS='clang;clang-tools-extra;lldb;lld'",
+    "-DLLVM_TARGETS_TO_BUILD='AArch64;ARM'",
+    "-DLLVM_TARGET_ARCH='AArch64'",
+    "-DLLVM_DEFAULT_TARGET_TRIPLE='aarch64-unknown-linux-gnu'",
+    "-DCMAKE_BUILD_TYPE='DEBUG'", "-DLLVM_ENABLE_ASSERTIONS='ON'",
+    "-DCMAKE_EXPORT_COMPILE_COMMANDS='ON'", "-DLLVM_CCACHE_BUILD='ON'",
+    "-DLLVM_ENABLE_LLD='ON'", "-DCMAKE_C_COMPILER='clang'",
+    "-DCMAKE_CXX_COMPILER='clang++'", "-DLLVM_ENABLE_ZLIB='ON'"]
+
+def run(args, stderr=None, stdout=None, shell=False, executable=None):
+    print("[ " + " ".join(args) + " ]\n")
+
+    subprocess.run(args=args, stderr=stderr, stdout=stdout,
+                   shell=shell, executable=executable)
 
 
 def configure_llvm():
-    CmdStr = "cmake -G Ninja " + \
-        " ".join(map(str, CMAKE_CONFIG_FLAGS)) + " ../llvm",
-    subprocess.run(CmdStr, shell=True, check=True)
+    run(["cmake", "-G", "Ninja", "-S", "./llvm", "-B", "./build"] + CMAKE_CONFIG_FLAGS)
 
 
 def build_llvm(target):
-    subprocess.run(["cmake", "--build", ".", "--target",
-                   target, "-j", "128"], check=True)
+    run(["cmake", "--build", "./build", "--target", target, "-j", "128"])
 
 
 if __name__ == "__main__":
-    os.chdir("build")
     match sys.argv[1]:
         case "config":
             configure_llvm()
