ed0e5f3acac9f665e59eae6abc8d1a30b387aa62
Paul <paul.heidekrueger@in.tum.de>
2023-03-24 16:13:18 +0100

    Throw away dependency chains which may get overwritten in function calls (#14)

---
+++ proj_bdo_llvm_build.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+import sys
+import subprocess
+
+CMAKE_CONFIG_FLAGS = [
+    "-DLLVM_ENABLE_PROJECTS='clang;clang-tools-extra;lldb;lld'",
+    "-DLLVM_TARGETS_TO_BUILD='AArch64;ARM'",
+    "-DLLVM_TARGET_ARCH='AArch64'",
+    "-DLLVM_DEFAULT_TARGET_TRIPLE='aarch64-unknown-linux-gnu'",
+    "-DCMAKE_BUILD_TYPE='RelWithDebInfo'", "-DLLVM_ENABLE_ASSERTIONS='ON'",
+    "-DCMAKE_EXPORT_COMPILE_COMMANDS='ON'", "-DLLVM_CCACHE_BUILD='ON'",
+    "-DLLVM_ENABLE_LLD='ON'", "-DCMAKE_C_COMPILER='clang'",
+    "-DCMAKE_CXX_COMPILER='clang++'", "-DLLVM_ENABLE_ZLIB='ON'"]
+
+def run(args, stderr=None, stdout=None, shell=False, executable=None):
+    print("[ " + " ".join(args) + " ]\n")
+
+    subprocess.run(args=args, stderr=stderr, stdout=stdout,
+                   shell=shell, executable=executable)
+
+
+def configure_llvm():
+    run(["cmake", "-G", "Ninja", "-S", "./llvm", "-B", "./build"] + CMAKE_CONFIG_FLAGS)
+
+
+def build_llvm(target):
+    run(["cmake", "--build", "./build", "--target", target, "-j", "128"])
+
+
+if __name__ == "__main__":
+    match sys.argv[1]:
+        case "config":
+            configure_llvm()
+        case "build":
+            build_llvm(sys.argv[2])
